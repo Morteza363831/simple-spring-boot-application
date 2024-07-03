560c9b9ba40000aa363fa19537dbd4e9eae90e7a
ruzstock.ir <ali16mar15couples@gmail.com>
2024-07-03 18:03:05 +0430

    flyway added . everything work well

---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE users(
+                      user_id INT NOT NULL PRIMARY KEY,
+                      name VARCHAR(255) NOT NULL,
+                      email VARCHAR(255) NOT NULL
+);
